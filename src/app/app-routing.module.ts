743ec2913e1822ad09de54b198ad7e313f99f922
juanforman <juanchiforman32@gmail.com>
2023-06-23 10:30:59 -0300

    creacion de rutas propias de auth y declaracion de componentes en module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { RegisterComponent } from './modules/auth/pages/register/register.component';
 
 const routes: Routes = [
-
-];
+{
+  path:"login", component:LoginComponent
+},
+{
+  path:"register", component:RegisterComponent
+},
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/nodules/auth/auth.module.ts
+++ src/app/nodules/auth/auth.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from 'src/app/modules/auth/pages/login/login.component';
+import { RegisterComponent } from 'src/app/modules/auth/pages/register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent,RegisterComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
   ]
 })
 export class AuthModule { }
